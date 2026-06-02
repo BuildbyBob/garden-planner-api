0e6d8f1169a556142435892f4c222600c3133e65
Bob van Veen <bobvanveen@kpnmail.nl>
2026-06-02 22:06:19 +0200

    Add initial project setup with variables exercise

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "tomaat";
+        int days = 2;
+        boolean isPlanted = true;
+        double time = 2.5;
+        System.out.println(name);
+        System.out.println(isPlanted);
+        System.out.println(time);
+        System.out.println(days);
+    }
+}
+
