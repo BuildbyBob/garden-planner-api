4d7cca7c9df223d6b0130cbf71b36aa9db83e5a8
Bob van Veen <bobvanveen@kpnmail.nl>
2026-06-04 20:15:39 +0200

    Add printPlant method with labeled output

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String name = "tomaat";
-        int days = 2;
-        boolean isPlanted = true;
-        double time = 2.5;
-        System.out.println(name);
-        System.out.println(isPlanted);
-        System.out.println(time);
-        System.out.println(days);
+        printPlant("tomaat", 2, true, 2.5);
+        printPlant("komkommer", 30, false, 0.8);
+    }
+
+    public static void printPlant(String name, int days, boolean isPlanted, double heightInMeters) {
+        System.out.println("Naam: " + name);
+        System.out.println("Days: " + days);
+        System.out.println("Is planted ? " + isPlanted);
+        System.out.println("Height in meters: " + heightInMeters);
     }
 }
 
